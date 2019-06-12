a6b93257e1b98ea44bfb8a6a71804015e6057bb9
MostafaIn <mostafa.hazareh@integrify.fi>
2019-06-12 17:10:58 +0300

    i have done 21 exercies in functions

--- Fundamental_JS/functions.js
+++ Fundamental_JS/functions.js
@@ -58,6 +58,7 @@ const capitalize = str => {
 }
 console.log(capitalize('i have a new car.'));
 
+
 /* 5. Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.*/
 const findLongest = str => {
     let strArr = str.match(/\w[a-z]{0,}/gi);
@@ -77,6 +78,7 @@ const longestWord = word => {
     return longest[longest.length - 1];
 };
 console.log(longestWord('I am a web Developer .'));
+
 
 /* 6. Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.*/
 const countVowels = str => {
@@ -104,6 +106,7 @@ const vowelsCount = str => {
     return counter;
 };
 console.log(vowelsCount('here is my home'));
+
 
 /* 7.  Write a JavaScript function that accepts a number as a parameter and check the number is prime or not. */
 const isPrime = n => {
@@ -141,6 +144,7 @@ console.log(typeCheck(null));
 console.log(typeCheck());
 console.log(typeCheck(true));
 
+
 /* 9. Write a JavaScript function which will take an array of numbers stored and find the second lowest and
  second greatest numbers, respectively.*/
 const secondLowestGreatest = numArr => {
@@ -162,6 +166,7 @@ const findSecondLowestGreatest = arr => {
 }
 console.log(findSecondLowestGreatest([33, 44, 66, 33, 66, 44, 11, 77]));
 
+
 /* 10. Write a JavaScript function to compute the factors of a positive integer. */
 const findFactor = n => {
     const factors = [];
@@ -174,6 +179,7 @@ const findFactor = n => {
 console.log(findFactor(15));
 console.log(findFactor(28));
 console.log(findFactor(17));
+
 
 /* 11. Write a JavaScript function to convert an amount to coins.*/
 const amountToCoins = (amount, coins) => {
@@ -189,5 +195,131 @@ const amountToCoins = (amount, coins) => {
 console.log(amountToCoins(46, [25, 5, 2, 1]));
 console.log(amountToCoins(13, [10, 5, 2, 1]));
 
-/* 12.  Write a JavaScript function to compute the value of bn where n is the exponent and b is the bases. Accept b and n from the user and display the result.*/
 
+/* 12.  Write a JavaScript function to compute the value of b **n where n is the exponent and b is the bases. Accept b and n from the user and display the result.*/
+const powered = (b, n) => b ** n;
+console.log(powered(3, 2));
+console.log(powered(5, 3));
+
+
+/* 13. Write a JavaScript function to extract unique characters from a string. */
+const uniqueChar = str => Array.from(new Set(str)).join('');
+console.log(uniqueChar('thequickbrownfoxjumpsoverthelazydog'));
+console.log(uniqueChar('this is my name here'));
+
+
+/* 14. Write a JavaScript function to  get the number of occurrences of each letter in specified string. */
+const countChar = str => {
+    let countArr = {};
+    str.replace(/\S/g, (l) => countArr[l] = (isNaN(countArr[l] ? 1 : countArr[l] + 1)));
+    return countArr;
+}
+console.log(countChar('the brown dog'))
+
+
+/* 15. Write a function for searching JavaScript arrays with a binary search. */
+const binarySearch = (arr, item) => {
+    let middleItem = Math.floor(arr.length / 2);
+    if (arr[middleItem] === item) {
+        return middleItem;
+    } else if (arr.length === 1) {
+        return null;
+    } else if (arr[middleItem] < item) {
+        let newArr = arr.slice(middleItem + 1);
+        let result = binarySearch(newArr, item);
+        if (result === null) {
+            return null;
+        } else {
+            return middleItem + 1 + result;
+        }
+    } else {
+        let arr1 = arr.slice(0, middleItem);
+        return binarySearch(arr1, item);
+    }
+};
+console.log(binarySearch([1, 2, 3, 4, 11, 22, 33, 44], 22));
+
+
+/* 16.Write a JavaScript function that returns array elements larger than a number. */
+const largerThanN = (arr, n) => {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > n) {
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
+console.log(largerThanN([22, 33, 11, 44, 55, 66], 33));
+
+/* 17. Write a JavaScript function that generates a string id (specified length) of random characters. */
+const makeID = n => {
+    let charList = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let id = ''
+    for (let i = 0; i < n; i++) {
+        id += charList.charAt(Math.floor(Math.random() * charList.length));
+    }
+    return id;
+}
+console.log(makeID(5));
+
+
+/* 18.  Write a JavaScript function to get all possible subset with a fixed length (for example 2) combinations in an array. */
+
+
+/* 19. Write a JavaScript function that accepts two arguments, a string and a letter and the function will count the number of occurrences of the specified letter within the string. */
+const occurrenceLetter = (str, char) => {
+    let count = 0;
+    for (character of str) {
+        if (character === char) count++;
+    }
+    return count;
+}
+console.log(occurrenceLetter('my name is mostafa', 's'));
+
+// second method
+const occurrenceLetter2 = (str, char) => {
+    let strs = str.split('');
+    let count = 0;
+    for (let i = 0; i < strs.length; i++) {
+        if (strs[i] === char) count++;
+    }
+    return count;
+}
+console.log(occurrenceLetter2('this is mostafa. I am a junior web developer.', 'e'));
+
+
+/* 20. Write a JavaScript function to find the first not repeated character. */
+const notRepeatedChar = (str) => {
+    for (let ch of str) {
+        if (str.indexOf(ch) === str.lastIndexOf(ch)) return ch;
+    }
+    return 'there is no unique character in here!';
+}
+console.log(notRepeatedChar('this is my name, that write here.'));
+console.log(notRepeatedChar('abacddbec'));
+
+
+/* 21. Write a JavaScript function to apply Bubble Sort algorithm. */
+const listArr = [23, 35456, 676, 34, 678, 24, 578, 3343, 684, 4, 35, 65];
+const bubbleSort = list => {
+    let temp;
+    for (let i = 0; i < list.length; i++) {
+        for (let j = i + 1; j < list.length; j++) {
+            if (list[i] < list[j]) {
+                temp = list[i]
+                list[i] = list[j]
+                list[j] = temp
+            }
+        }
+    }
+    return list;
+};
+console.log(bubbleSort(listArr));
+
+// second method
+const sorting = list => list.sort((a,b) => b - a);
+console.log(sorting(listArr));
+
+
+/* 22. Write a JavaScript function that accept a list of country names as input and returns the longest country name as output. */
