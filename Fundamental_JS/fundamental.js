65119109905b9d7eaf3ffd9fb9431fb88c0bc1df
MostafaIn <mostafa.hazareh@integrify.fi>
2019-06-14 16:18:11 +0300

    start to practice in fundamentals

--- Fundamental_JS/fundamental.js
+++ Fundamental_JS/fundamental.js
@@ -4,9 +4,7 @@
 
 /* Write a JavaScript program to compare two objects to determine if the first one contains equivalent property values to the second one. */
 // method 1.
-const compareObjects = (obj, source) => {
-    return Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
-}
+const compareObjects = (obj, source) => Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
 objectOne = {
     name: "mostafa",
     age: 34,
