48a49a2e58ed4a3f0d38f6267af8fc57439c32b9
MostafaIn <mostafa.hazareh@integrify.io>
2019-11-20 17:05:52 +0200

    count items of an array

--- interviewFunc.js
+++ interviewFunc.js
@@ -1,4 +1,4 @@
-// A. a javascript program to remove the repeated words and return clear sentences.
+// 1. a javascript program to remove the repeated words and return clear sentences.
 
 const sentences = 'this is me this me is mostafa me mostafa this this';
 
@@ -21,7 +21,7 @@ const removeRepeatedWords2 = (str) => {
 };
 console.log(removeRepeatedWords2(sentences));
 
-// B. this program create and array of objects with merging two array
+// 2. this program create and array of objects with merging two array
 let names = ['mosi', 'rohy', 'edi', 'mori', 'dany', 'ss'];
 let ages = [34, 32, 30, 31, 2];
 
@@ -40,3 +40,42 @@ const returnObj = () => {
 	return list.filter((e) => e.age < 31);
 };
 console.log(returnObj());
+
+// 3. a javascript program to count the items of an array and return an object of any item and its count.
+const myFruits = [
+	'apple',
+	'mango',
+	'ananas',
+	'banana',
+	'apple',
+	'apple',
+	'ananas',
+	'mango',
+	'apple',
+	'orange',
+	'mango',
+	'orange',
+	'banana',
+	'banana',
+	'graps',
+];
+// first option
+const countItems = (arr) => {
+	return arr.reduce((countItem, item) => {
+		countItem[item] = (countItem[item] || 0) + 1;
+		return countItem;
+	}, {});
+};
+console.log(countItems(myFruits));
+
+// second option
+const countItem2 = (arr) => {
+	const itemsCount = {};
+	for (let item of arr) {
+		itemsCount[item] = itemsCount[item] ? itemsCount[item] + 1 : 1;
+	}
+	return itemsCount;
+};
+console.log(countItem2(myFruits));
+
+//
