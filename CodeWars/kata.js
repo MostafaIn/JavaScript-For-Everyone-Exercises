d786b828c9a088646afdf316b81982d40295353b
mostafaIn <mostafa2015se@gmail.com>
2020-03-02 23:37:44 +0200

    kata 2

--- CodeWars/kata.js
+++ CodeWars/kata.js
@@ -8,5 +8,10 @@ console.log(isSquare(22));
 console.log(isSquare(64));
 
 
-// 2. 
+// 2. Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+const sheepsList = [true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true,  false, false, true , true,  true,  true,  true , false, false, true,  true]
 
+const countList = () =>{
+    return sheepsList.filter(Boolean).length
+};
+console.log(countList())
